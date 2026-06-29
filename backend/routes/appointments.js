be1ecacc71e4fccd1f519e51cb467f47818c4daf
ashritha <ashrithaduppati2@gmail.com>
2026-06-29 18:02:05 +0530

    remove: cleanup bulk delete endpoint

--- backend/routes/appointments.js
+++ backend/routes/appointments.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { createAppointment, getAllAppointments, getAppointment, updateAppointment, deleteAppointment, getStats, getAvailableSlots, deleteAllAppointments } = require('../controllers/appointmentController');
+const { createAppointment, getAllAppointments, getAppointment, updateAppointment, deleteAppointment, getStats, getAvailableSlots } = require('../controllers/appointmentController');
 const { protect } = require('../middleware/auth');
 const { body } = require('express-validator');
 const { validate } = require('../middleware/validate');
@@ -20,7 +20,6 @@ router.get('/', protect, getAllAppointments);
 router.get('/stats', protect, getStats);
 router.get('/:id', protect, getAppointment);
 router.put('/:id', protect, updateAppointment);
-router.delete('/delete-all', protect, deleteAllAppointments);
 router.delete('/:id', protect, deleteAppointment);
 
 module.exports = router;
